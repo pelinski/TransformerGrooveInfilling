602a51d2710a19c1572c30734d1882f3a95a25be
Teresa Pelinski <teresapelinski@gmail.com>
2021-05-28 18:52:06 +0200

    config for it1 training

--- model/train_infilling.py
+++ model/train_infilling.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     filters = {
         "beat_type": ["beat"],
         "time_signature": ["4-4"],
-        "master_id": ["drummer9/session1/8"]
+#        "master_id": ["drummer9/session1/8"]
     }
 
     subset_info = {
@@ -59,11 +59,11 @@ if __name__ == "__main__":
         'max_len': 32,
         'mso_parameters': {'sr': 44100, 'n_fft': 1024, 'win_length': 1024, 'hop_length':
             441, 'n_bins_per_octave': 16, 'n_octaves': 9, 'f_min': 40, 'mean_filter_size': 22},
-        'voices_parameters': {'voice_idx': [0, 1], 'min_n_voices_to_remove': 1,
-                              'max_n_voices_to_remove': 2, 'prob': [1, 1], 'k': 5},
-        'sf_path': '../soundfonts/filtered_soundfonts/',
-        'max_n_sf': None,
-        'max_aug_items': 10,
+        'voices_parameters': {'voice_idx': [2], 'min_n_voices_to_remove': 1,    # closed hh
+                              'max_n_voices_to_remove': 1, 'prob': [1], 'k': None},
+        'sf_path': '../soundfonts/filtered_soundfonts/Standard_Drum_Kit.sf2',
+        'max_n_sf': 1,
+        'max_aug_items': 1,
         'dataset_name': None
     }
 
