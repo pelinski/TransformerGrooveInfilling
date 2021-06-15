18bc89d8debeb705909202172005ba7fd6e85799
Teresa Pelinski <teresapelinski@gmail.com>
2021-06-15 13:24:19 +0200

    updated dataset_test

--- model/tests/dataset_test.py
+++ model/tests/dataset_test.py
@@ -1,53 +1,78 @@
 import sys
+import torch
 sys.path.insert(1, "../")
 sys.path.append('../../../hvo_sequence/')
 sys.path.append('../../../preprocessed_dataset/')
 
-from dataset import GrooveMidiDataset
+from dataset import GrooveMidiDatasetInfilling
 from Subset_Creators.subsetters import GrooveMidiSubsetter
 import numpy as np
 
 if __name__ == "__main__":
-    #GMD
-    # load subset
-    filters = {"beat_type": ["beat"],
-               "master_id": ["drummer1/eval_session/10"]}
-    # subset creator
-    pickle_source_path = '../../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.3.0/Processed_On_13_05_2021_at_12_56_hrs'
-    subset_name = 'GrooveMIDI_processed_test'
-    metadata_csv_filename = 'metadata.csv'
-    hvo_pickle_filename = 'hvo_sequence_data.obj'
 
-    gmd_subsetter = GrooveMidiSubsetter(
-        pickle_source_path=pickle_source_path,
-        subset=subset_name,
-        hvo_pickle_filename=hvo_pickle_filename,
-        list_of_filter_dicts_for_subsets=[filters],
-    )
-    _, subset_list = gmd_subsetter.create_subsets()
-
-    subset_info = {"pickle_source_path": pickle_source_path,
-                   "subset": subset_name,
-                   "metadata_csv_filename": metadata_csv_filename,
-                   "hvo_pickle_filename": hvo_pickle_filename,
-                   "filters": filters}
-
-    mso_parameters = {
-        "sr": 44100,
-        "n_fft": 1024,
-        "win_length": 1024,
-        "hop_length": 441,
-        "n_bins_per_octave": 16,
-        "n_octaves": 9,
-        "f_min": 40,
-        "mean_filter_size": 22
+    params = {
+        "model": {
+            'optimizer': 'sgd',
+            'd_model': 128,
+            'n_heads': 8,
+            'dim_feedforward': 1280,
+            'dropout': 0.1,
+            'num_encoder_layers': 5,
+            'num_decoder_layers': 5,
+            'max_len': 32,
+            'embedding_size_src': 16,  # mso
+            'embedding_size_tgt': 27,  # hvo
+            'device': 'cuda' if torch.cuda.is_available() else 'cpu'
+        },
+        "training": {
+            'learning_rate': 1e-3,
+            'batch_size': 64,
+            'lr_scheduler_step_size': 30,
+            'lr_scheduler_gamma': 0.1
+        },
+        "dataset": {
+            "pickle_source_path": '../../../preprocessed_dataset/datasets_extracted_locally/GrooveMidi/hvo_0.4.5/Processed_On_14_06_2021_at_14_26_hrs',
+            "subset": 'GrooveMIDI_processed_train',
+            "metadata_csv_filename": 'metadata.csv',
+            "hvo_pickle_filename": 'hvo_sequence_data.obj',
+            "filters": {
+                "beat_type": ["beat"],
+                "time_signature": ["4-4"],
+                # "master_id": ["drummer9/session1/8"]
+                "master_id": ["drummer1/session1/201"]
+            },
+            'max_len': 32,
+            'mso_params': {'sr': 44100, 'n_fft': 1024, 'win_length': 1024, 'hop_length':
+                441, 'n_bins_per_octave': 16, 'n_octaves': 9, 'f_min': 40, 'mean_filter_size': 22},
+            'voices_params': {'voice_idx': [2], 'min_n_voices_to_remove': 1,  # closed hh
+                              'max_n_voices_to_remove': 1, 'prob': [1], 'k': None},
+            'sf_path': ['../../soundfonts/filtered_soundfonts/Standard_Drum_Kit.sf2'],
+            'max_n_sf': 1,
+            'max_aug_items': 1,
+            'dataset_name': None
+        },
+        "evaluator": {"n_samples_to_use": 12,
+                      "n_samples_to_synthesize_visualize_per_subset": 10},
+        "cp_paths": {
+            'checkpoint_path': '../train_results/',
+            'checkpoint_save_str': '../train_results/transformer_groove_infilling-epoch-{}'
+        },
+        "load_model": None,
     }
+
+    #GMD
+
+    _, subset_list = GrooveMidiSubsetter(pickle_source_path=params["dataset"]["pickle_source_path"],
+                                         subset=params["dataset"]["subset"],
+                                         hvo_pickle_filename=params["dataset"]["hvo_pickle_filename"],
+                                         list_of_filter_dicts_for_subsets=[
+                                             params['dataset']['filters']]).create_subsets()
 
     # check that inputs are mso
     check_inputs = False
     if check_inputs:
         # load gmd
-        gmd = GrooveMidiDataset(subset=subset_list[0], subset_info=subset_info, mso_parameters=mso_parameters)
+        gmd = GrooveMidiDatasetInfilling(data=subset_list[0], **params['dataset'])
         # check that input corresponds to mso
         _in, _, _ = gmd.__getitem__(1)
         hvo_in = gmd.get_hvo_sequence(1)
@@ -56,19 +81,7 @@ if __name__ == "__main__":
         mso = hvo_in_reset.mso(sf_path=gmd.get_soundfont(1))
         print(np.sum(_in.numpy() - mso > 1e-5))
 
-    ## test dataset with kwargs
-    dataset_parameters = {
-        'max_len': 32,
-        'mso_parameters': {'sr': 44100, 'n_fft': 1024, 'win_length': 1024, 'hop_length':
-            441, 'n_bins_per_octave': 16, 'n_octaves': 9, 'f_min': 40, 'mean_filter_size': 22},
-        'voices_parameters': {'voice_idx': [0, 1], 'min_n_voices_to_remove': 1,
-                              'max_n_voices_to_remove': 2, 'prob': [1, 1], 'k': 5},
-        'sf_path': '../../soundfonts/filtered_soundfonts/',
-        'max_n_sf': None,
-        'max_aug_items': 10,
-        'dataset_name': "test_kwargs"
-    }
-    gmd = GrooveMidiDataset(subset=subset_list[0], subset_info=subset_info, **dataset_parameters)
+    gmd = GrooveMidiDatasetInfilling(data=subset_list[0], **params['dataset'])
     print(gmd.__len__(), len(gmd.hvo_sequences))
     hvo = gmd.get_hvo_sequence(1)
     print(hvo.get_active_voices())
