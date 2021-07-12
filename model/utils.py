be914b47d213db0a28663d39c7bc2d35ac8aa101
Teresa Pelinski <teresapelinski@gmail.com>
2021-07-12 03:37:50 +0200

    updated evaluator to 0.2.0, now working with augmented data (multiple sfs and removed voices)

--- model/utils.py
+++ model/utils.py
@@ -30,8 +30,8 @@ def get_sf_list(sf_path):
 
 def get_hvo_idx_for_voice(voice_idx, n_voices):
     h_idx = voice_idx
-    v_idx = voice_idx + n_voices
-    o_idx = voice_idx + 2 * n_voices
+    v_idx = [_ + n_voices for _ in voice_idx]
+    o_idx = [_ + 2 * n_voices for _ in voice_idx]
 
     return h_idx, v_idx, o_idx
 
