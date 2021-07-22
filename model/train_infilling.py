5ed19f0f644e38a8d32c3262dbf13a64e80550e9
Teresa Pelinski <teresapelinski@gmail.com>
2021-07-22 16:41:01 +0200

    added evaluator loading from pickle file

--- model/train_infilling.py
+++ model/train_infilling.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import wandb
+import pickle
 from torch.utils.data import DataLoader
 
 import sys
@@ -12,13 +13,19 @@ from evaluator import InfillingEvaluator
 from hvo_sequence.drum_mappings import ROLAND_REDUCED_MAPPING
 from models.train import initialize_model, calculate_loss, train_loop
 from utils import get_epoch_log_freq
-from preprocess_infilling_dataset import load_preprocessed_dataset
+from preprocess_dataset import load_preprocessed_dataset
 
 # ================================= SETTINGS ==================================================== #
-preprocessed_dataset_path_train = '../preprocessed_infilling_datasets/InfillingKicksAndSnares_testing/0.1.2/train'
-preprocessed_dataset_path_test = '../preprocessed_infilling_datasets/InfillingKicksAndSnares_testing/0.1.2/test'
-#preprocessed_dataset_path_train = '../preprocessed_infilling_datasets/InfillingKicksAndSnares_testing/0.1.2/train'
-#preprocessed_dataset_path_test = '../preprocessed_infilling_datasets/InfillingKicksAndSnares_testing/0.1.2/test'
+preprocessed_dataset_path_train = '../datasets/InfillingKicksAndSnares_testing/0.1.2/train'
+preprocessed_dataset_path_test = '../datasets/InfillingKicksAndSnares_testing/0.1.2/test'
+evaluator_train_file = '../evaluators/InfillingKicksAndSnares_testing/InfillingKicksAndSnares_train_0.1.2_evaluator.pickle'
+evaluator_test_file = '../evaluators/InfillingKicksAndSnares_testing/InfillingKicksAndSnares_test_0.1.2_evaluator' \
+                       '.pickle'
+
+# preprocessed_dataset_path_train = '../preprocessed_infilling_datasets/InfillingKicksAndSnares_testing/0.1.2/train'
+# preprocessed_dataset_path_test = '../preprocessed_infilling_datasets/InfillingKicksAndSnares_testing/0.1.2/test'
+
+# TODO select experiment here
 
 settings = {'log_to_wandb': True,
             'evaluator_test': True,
@@ -48,8 +55,8 @@ hyperparameter_defaults = dict(
     #    lr_scheduler_step_size=30,
     #    lr_scheduler_gamma=0.1
 )
-wandb_run = wandb.init(config=hyperparameter_defaults, project=hyperparameter_defaults['experiment']+'_testing',
-                       job_type=settings['job_type'],settings=wandb.Settings(start_method="fork"))
+wandb_run = wandb.init(config=hyperparameter_defaults, project=hyperparameter_defaults['experiment'] + '_testing',
+                       job_type=settings['job_type'], settings=wandb.Settings(start_method="fork"))
 
 params = {
     "model": {
@@ -76,8 +83,6 @@ params = {
         #        'lr_scheduler_step_size': wandb.config.lr_scheduler_step_size,
         #        'lr_scheduler_gamma': wandb.config.lr_scheduler_gamma
     },
-    "evaluator": {"n_samples_to_use": 11,  # 2048
-                  "n_samples_to_synthesize_visualize_per_subset": 10},  # 10
     "cp_paths": {
         'checkpoint_path': '../train_results/' + hyperparameter_defaults['experiment'],
         'checkpoint_save_str': '../train_results/' + hyperparameter_defaults[
@@ -101,22 +106,8 @@ if wandb.config.use_evaluator:
 
     pred_horizontal = False if wandb.config.experiment == 'InfillingRandom' else True
 
-    evaluator_train = InfillingEvaluator(
-        pickle_source_path=dataset_train.subset_info["pickle_source_path"],
-        set_subfolder=dataset_train.subset_info["subset"],
-        hvo_pickle_filename=dataset_train.subset_info["hvo_pickle_filename"],
-        max_hvo_shape=(32, 27),
-        n_samples_to_use=params["evaluator"]["n_samples_to_use"],
-        n_samples_to_synthesize_visualize_per_subset=params["evaluator"][
-            "n_samples_to_synthesize_visualize_per_subset"],
-        _identifier='Train_Set',
-        disable_tqdm=False,
-        analyze_heatmap=True,
-        analyze_global_features=True,
-        dataset=dataset_train,
-        model=model,
-        n_epochs=wandb.config.epochs,
-        horizontal=pred_horizontal)
+    with open(evaluator_train_file, 'rb') as f:
+        evaluator_train = pickle.load(f)
 
     # log eval_subset parameters to wandb
     wandb.config.update({"train_hvo_index": evaluator_train.hvo_index,
@@ -127,22 +118,8 @@ if wandb.config.use_evaluator:
     if settings['evaluator_test']:
         dataset_test = load_preprocessed_dataset(preprocessed_dataset_path_test, exp=wandb.config.experiment)
 
-        evaluator_test = InfillingEvaluator(
-            pickle_source_path=dataset_test.subset_info["pickle_source_path"],
-            set_subfolder=dataset_test.subset_info["subset"],
-            hvo_pickle_filename=dataset_test.subset_info["hvo_pickle_filename"],
-            max_hvo_shape=(32, 27),
-            n_samples_to_use=params["evaluator"]["n_samples_to_use"],
-            n_samples_to_synthesize_visualize_per_subset=params["evaluator"][
-                "n_samples_to_synthesize_visualize_per_subset"],
-            _identifier='Test_Set',
-            disable_tqdm=False,
-            analyze_heatmap=True,
-            analyze_global_features=True,
-            dataset=dataset_test,
-            model=model,
-            n_epochs=wandb.config.epochs,
-            horizontal=pred_horizontal)
+        with open(evaluator_test_file, 'rb') as f:
+            evaluator_test = pickle.load(f)
 
         # log eval_subset parameters to wandb
         wandb.config.update({"test_hvo_index": evaluator_test.hvo_index,
@@ -165,7 +142,7 @@ for i in range(eps):
         "encoder_only"], opt=optimizer, epoch=ep, loss_fn=calculate_loss, bce_fn=BCE_fn,
                mse_fn=MSE_fn, save=save_model, device=params["model"]['device'],
                test_inputs=evaluator_test.processed_inputs,
-               test_gt = evaluator_test.processed_gt,
+               test_gt=evaluator_test.processed_gt,
                h_loss_mult=params["model"]["h_loss_multiplier"],
                v_loss_mult=params["model"]["v_loss_multiplier"],
                o_loss_mult=params["model"]["o_loss_multiplier"])
@@ -174,7 +151,7 @@ for i in range(eps):
         if i in epoch_save_partial or i in epoch_save_all:
             # Train set evaluator
             evaluator_train._identifier = 'Train_Set'
-            evaluator_train.set_pred()
+            evaluator_train.set_pred(model)
             train_acc_h = evaluator_train.get_hits_accuracies(drum_mapping=ROLAND_REDUCED_MAPPING)
             train_mse_v = evaluator_train.get_velocity_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
             train_mse_o = evaluator_train.get_micro_timing_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
@@ -193,7 +170,7 @@ for i in range(eps):
             if settings['evaluator_test']:
                 # Test set evaluator
                 evaluator_test._identifier = 'Test_Set'
-                evaluator_test.set_pred()
+                evaluator_test.set_pred(model)
                 test_acc_h = evaluator_test.get_hits_accuracies(drum_mapping=ROLAND_REDUCED_MAPPING)
                 test_mse_v = evaluator_test.get_velocity_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
                 test_mse_o = evaluator_test.get_micro_timing_errors(drum_mapping=ROLAND_REDUCED_MAPPING)
