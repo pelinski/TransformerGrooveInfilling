8b58c28b1f1816ddc6b133a7c5617cf20096c9a0
Teresa Pelinski <teresapelinski@gmail.com>
2021-07-24 00:49:32 +0200

    fixed bug that loaded InfillingRandom dataset as Infilling dataset

--- model/preprocess_evaluator.py
+++ model/preprocess_evaluator.py
@@ -1,4 +1,4 @@
-import copy
+import torch
 from evaluator import InfillingEvaluator
 from preprocess_dataset import load_preprocessed_dataset
 
@@ -8,38 +8,42 @@ params = {
             "train": '../datasets/InfillingKicksAndSnares/0.1.2/train',
             'test': '../datasets/InfillingKicksAndSnares/0.1.2/test'
         },
-        "InfillingRandom" : {
-            "train":  '../datasets/InfillingRandom/0.0.0/train',
-            'test':  '../datasets/InfillingRandom/0.0.0/test'
+        "InfillingRandom": {
+            "train": '../datasets/InfillingRandom/0.0.0/train',
+            'test': '../datasets/InfillingRandom/0.0.0/test'
         },
         "InfillingKicksAndSnares_testing": {
-            "train": '../datasets/InfillingKicksAndSnares_testing/0.1.2/train',
-            'test': '../datasets/InfillingKicksAndSnares_testing/0.1.2/test'
+            "train": '../datasets/InfillingKicksAndSnares_testing/0.1.3/train',
+            'test': '../datasets/InfillingKicksAndSnares_testing/0.1.3/test'
         },
         "InfillingRandom_testing":
             {
-                "train":'../datasets/InfillingRandom_testing/0.0.0/train',
-                'test': '../datasets/InfillingRandom_testing/0.0.0/test'
+                "train": '../datasets/InfillingRandom_testing/0.0.1/train',
+                'test': '../datasets/InfillingRandom_testing/0.0.1/test'
             }
     },
     "evaluator": {
-        "n_samples_to_use": 2048,  # 2048
-        "n_samples_to_synthesize_visualize_per_subset": 10,
+        "n_samples_to_use": 10,  # 2048
+        "n_samples_to_synthesize_visualize_per_subset": 5,  # 10
         "save_evaluator_path": '../evaluators/'}}
 
 if __name__ == "__main__":
-    exps = ['InfillingKicksAndSnares']
+
+    testing = True
+
+    exps = ['InfillingRandom']
     splits = ['train', 'test']
     for exp in exps:
         print('------------------------\n' + exp + '\n------------------------\n')
         for split in splits:
             print('Split: ', split)
-            dataset = load_preprocessed_dataset(params["dataset_paths"][exp][split], exp=exp)
 
-            params_exp = copy.deepcopy(params)
-            params_exp['evaluator']['save_evaluator_path'] = params_exp['evaluator']['save_evaluator_path'] + exp + '/'
+            _exp = exp + '_testing' if testing else exp
 
-            pred_horizontal = False if (exp == 'InfillingRandom' or exp == 'InfillingRandom_testing') else True
+            pred_horizontal = False if exp == 'InfillingRandom' else True
+
+            dataset = load_preprocessed_dataset(params["dataset_paths"][_exp][split], exp=exp)  # FIXME
+
             evaluator = InfillingEvaluator(
                 pickle_source_path=dataset.subset_info["pickle_source_path"],
                 set_subfolder=dataset.subset_info["subset"],
@@ -53,6 +57,8 @@ if __name__ == "__main__":
                 analyze_heatmap=True,
                 analyze_global_features=False,  # pdf
                 dataset=dataset,
-                horizontal=pred_horizontal)
+                horizontal=pred_horizontal,
+                device='cuda' if torch.cuda.is_available() else 'cpu')
 
-            evaluator.save_as_pickle(save_evaluator_path=params_exp['evaluator']['save_evaluator_path'])
+            save_evaluator_path = params['evaluator']['save_evaluator_path'] + _exp + '/' + dataset.__version__ + '/'
+            evaluator.save_as_pickle(save_evaluator_path=save_evaluator_path)
