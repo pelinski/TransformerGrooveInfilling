4e29f5971439557efc4898bc731f7056e50a9a6f
Teresa Pelinski <teresapelinski@gmail.com>
2021-07-29 14:14:24 +0200

    added argparser
    Former-commit-id: 8d631070ee2594a4f015d5996c02a504c2df7baa [formerly 8cdeb6302cded2485609fac79dd5f15a95ab446c]
    Former-commit-id: 8d631070ee2594a4f015d5996c02a504c2df7baa

--- model/train.py
+++ model/train.py
@@ -3,6 +3,9 @@ import torch
 import wandb
 from torch.utils.data import DataLoader
 import sys
+import yaml
+import argparse
+import pprint
 
 from preprocess_dataset import load_preprocessed_dataset
 from evaluator import init_evaluator, log_eval
@@ -11,101 +14,30 @@ from utils import get_epoch_log_freq
 sys.path.insert(1, "../../BaseGrooveTransformers/")
 from models.train import initialize_model, calculate_loss, train_loop
 
-experiment = 'InfillingClosedHH'
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", help="yaml config file", default='configs/hyperparameter_defaults.yaml')
+parser.add_argument("--paths", help="paths file", default='configs/paths.yaml')
+parser.add_argument("--testing", help="testing mode", default=False)
+parser.add_argument("--wandb", help="log to wandb", default=True)
+parser.add_argument("--eval_train", help="evaluator train set", default=True)
+parser.add_argument("--eval_test", help="evaluator test set", default=True)
+args = parser.parse_args()
 
-settings = {'testing': False,
-            'log_to_wandb': True,
-            'evaluator_train': True,
-            'evaluator_test': True,
-            'job_type': 'train'}
+with open(args.config, 'r') as f:
+    hyperparameters = yaml.safe_load(f)
+if args.testing:
+    hyperparameters['epochs'] = 1
+pprint.pprint(hyperparameters)
 
-# –––––––––––––––––––––––––––––––––––––––––––––––––––
-
-hyperparameter_defaults = dict(
-    experiment=experiment,
-    encoder_only=1,
-    optimizer_algorithm='sgd',
-    d_model=64,
-    n_heads=16,
-    dropout=0.2,
-    num_encoder_decoder_layers=7,
-    hit_loss_penalty=1,
-    batch_size=16,
-    dim_feedforward=256,
-    learning_rate=0.05,
-    epochs=1 if settings['testing'] else 150,
-    #    lr_scheduler_step_size=30,
-    #    lr_scheduler_gamma=0.1
-)
-
-paths = {
-    "InfillingKicksAndSnares": {
-        'datasets': {
-            "train": '../datasets/InfillingKicksAndSnares/0.1.2/train',
-            "test": '../datasets/InfillingKicksAndSnares/0.1.2/test'},
-        'evaluators': {
-            'train': '../evaluators/InfillingEvaluator_0.3.2/InfillingKicksAndSnares_train_0.1.2_evaluator.pickle',
-            'test': '../evaluators/InfillingEvaluator_0.3.2/InfillingKicksAndSnares_test_0.1.2_evaluator.pickle'
-        }
-    },
-    "InfillingRandom": {
-        'datasets': {
-            "train": '../datasets/InfillingRandom/0.0.0/train',
-            "test": '../datasets/InfillingRandom/0.0.0/test'},
-        'evaluators': {
-            'train': '../evaluators/InfillingEvaluator_0.3.2/InfillingRandom_train_0.0.0_evaluator.pickle',
-            'test': '../evaluators/InfillingEvaluator_0.3.2/InfillingRandom_test_0.0.0_evaluator.pickle'
-        },
-    },
-    "InfillingClosedHH": {
-        'datasets': {
-            "train": '../datasets/InfillingClosedHH/0.1.2/train',
-            "test": '../datasets/InfillingClosedHH/0.1.2/test'},
-        'evaluators': {
-            'train': '../evaluators/InfillingEvaluator_0.3.2/InfillingClosedHH_train_0.1.2_evaluator.pickle',
-            'test': '../evaluators/InfillingEvaluator_0.3.2/InfillingClosedHH_test_0.1.2_evaluator'
-                    '.pickle'
-        }
-    },
-    "InfillingKicksAndSnares_testing": {
-        'datasets': {
-            "train": '../datasets/InfillingKicksAndSnares_testing/0.1.3/train',
-            "test": '../datasets/InfillingKicksAndSnares_testing/0.1.3/test'},
-        'evaluators': {
-            'train': '../evaluators/InfillingEvaluator_0.3.2/InfillingKicksAndSnares_testing_train_0.1.3_evaluator.pickle',
-            'test': '../evaluators/InfillingEvaluator_0.3.2/InfillingKicksAndSnares_testing_test_0.1.3_evaluator'
-                    '.pickle'
-        }
-    },
-    "InfillingRandom_testing": {
-        'datasets': {
-            "train": '../datasets/InfillingRandom_testing/0.0.0/train',
-            "test": '../datasets/InfillingRandom_testing/0.0.0/test'},
-        'evaluators': {
-            'train': '../evaluators/InfillingEvaluator_0.3.2/InfillingRandom_testing_train_0.0.0_evaluator.pickle',
-            'test': '../evaluators/InfillingEvaluator_0.3.2/InfillingRandom_testing_test_0.0.0_evaluator.pickle'
-        }
-    },
-    "InfillingClosedHH_testing": {
-        'datasets': {
-            "train": '../datasets/InfillingClosedHH_testing/0.1.2/train',
-            "test": '../datasets/InfillingClosedHH_testing/0.1.2/test'},
-        'evaluators': {
-            'train': '../evaluators/InfillingEvaluator_0.3.2/InfillingClosedHH_testing_train_0.1.2_evaluator.pickle',
-            'test': '../evaluators/InfillingEvaluator_0.3.2/InfillingClosedHH_testing_test_0.1.2_evaluator'
-                    '.pickle'
-        }
-    },
-}
-
-# –––––––––––––––––––––––––––––––––––––––––––––––––––
+with open(args.paths, 'r') as f:
+    paths = yaml.safe_load(f)
 
 if __name__ == '__main__':
-    os.environ['WANDB_MODE'] = 'online' if settings['log_to_wandb'] else 'offline'
+    os.environ['WANDB_MODE'] = 'online' if args.wandb else 'offline'
 
-    wandb.init(config=hyperparameter_defaults,
-               project=experiment,
-               job_type=settings['job_type'],
+    wandb.init(config=hyperparameters,
+               project=hyperparameters['experiment'],
+               job_type='train',
                settings=wandb.Settings(start_method="fork"))
 
     params = {
@@ -146,11 +78,11 @@ if __name__ == '__main__':
                                               exp=wandb.config.experiment)
     dataloader_train = DataLoader(dataset_train, batch_size=wandb.config.batch_size, shuffle=True, pin_memory=True)
 
-    if settings['evaluator_train']:
-        evaluator_train = init_evaluator(paths[wandb.config.experiment]['evaluators']['train'], device= params[
+    if args.eval_train:
+        evaluator_train = init_evaluator(paths[wandb.config.experiment]['evaluators']['train'], device=params[
             'model']['device'])
-    if settings['evaluator_test']:
-        evaluator_test = init_evaluator(paths[wandb.config.experiment]['evaluators']['test'],device= params[
+    if args.eval_test:
+        evaluator_test = init_evaluator(paths[wandb.config.experiment]['evaluators']['test'], device=params[
             'model']['device'])
 
     eps = wandb.config.epochs
@@ -172,22 +104,22 @@ if __name__ == '__main__':
                    bce_fn=BCE_fn,
                    mse_fn=MSE_fn,
                    device=params["model"]['device'],
-                   test_inputs=evaluator_test.processed_inputs if settings['evaluator_test'] else None,
-                   test_gt=evaluator_test.processed_gt if settings['evaluator_test'] else None,
+                   test_inputs=evaluator_test.processed_inputs if args.eval_test else None,
+                   test_gt=evaluator_test.processed_gt if args.eval_test else None,
                    hit_loss_penalty=wandb.config.hit_loss_penalty,
                    save=(i in epoch_save_partial or i in epoch_save_all))
         print("-------------------------------\n")
 
         if i in epoch_save_partial or i in epoch_save_all:
-            if settings['evaluator_train']:
-                #evaluator_train._identifier = 'Train_Set_Epoch_{}'.format(ep) # FIXME
+            if args.eval_train:
+                # evaluator_train._identifier = 'Train_Set_Epoch_{}'.format(ep) # FIXME
                 evaluator_train._identifier = 'Train_Set'
-                log_eval(evaluator_train, model, log_media=i in epoch_save_all, epoch=ep, dump=not settings['testing'])
+                log_eval(evaluator_train, model, log_media=i in epoch_save_all, epoch=ep, dump=not args.testing)
 
-            if settings['evaluator_test']:
+            if args.eval_test:
                 evaluator_test._identifier = 'Test_Set'
-                #evaluator_test._identifier = 'Test_Set_Epoch_{}'.format(ep) # FIXME
-                log_eval(evaluator_test, model, log_media=i in epoch_save_all, epoch=ep, dump=not settings['testing'])
+                # evaluator_test._identifier = 'Test_Set_Epoch_{}'.format(ep) # FIXME
+                log_eval(evaluator_test, model, log_media=i in epoch_save_all, epoch=ep, dump=not args.testing)
 
         wandb.log({"epoch": ep}, commit=True)
 
