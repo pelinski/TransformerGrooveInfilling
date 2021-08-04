01049661007abb41a65114a5d460376a300d4e25
Teresa Pelinski <teresapelinski@gmail.com>
2021-08-05 01:23:41 +0200

    minor changes
    Former-commit-id: 233f1cbbe73f21847f6ab37a8e91db91cbab0b3a [formerly 16117720336b9d5edbf711cd02eb7b8d093330b1]
    Former-commit-id: 233f1cbbe73f21847f6ab37a8e91db91cbab0b3a

--- model/train.py
+++ model/train.py
@@ -58,7 +58,8 @@ else:
         batch_size=args.batch_size,
         dim_feedforward=args.dim_feedforward,
         learning_rate=args.learning_rate,
-        epochs=args.epochs)
+        epochs=args.epochs,
+        load_model=args.load_model)
 
 if args.testing:
     hyperparameters['epochs'] = 1
@@ -105,7 +106,7 @@ if __name__ == '__main__':
             #        'lr_scheduler_step_size': 30,
             #        'lr_scheduler_gamma': 0.1
         },
-        "load_model": args.load_model
+        "load_model": wandb.config.load_model
     }
 
     # log params to wandb
